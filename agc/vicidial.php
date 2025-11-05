71057edf9dcf1aeecf66e943de058512595f8297
simpl <simpl@SIMPL-FVFHK4LNQ05N.local>
2025-11-05 16:53:11 +0530

    Update1

--- agc/vicidial.php
+++ agc/vicidial.php
@@ -24032,7 +24032,7 @@ $zi=2;
                 onmouseover="this.style.background='#059669';"
                 onmouseout="this.style.background='#10b981';">
                 <svg style="width:12px;height:12px;fill:#fff;" viewBox="0 0 24 24"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>
-                COMMIT
+                
             </button>
         </div>
     </div>
