b1206cb63307649abb501604e26aee1ac1ed5749
simpl <simpl@SIMPL-FVFHK4LNQ05N.local>
2025-11-05 16:55:13 +0530

    Update1

--- agc/vicidial.php
+++ agc/vicidial.php
@@ -19276,7 +19276,8 @@ function set_length(SLnumber,SLlength_goal,SLdirection)
 							//	alert(xmlhttp.responseText);
 								needToConfirmExit = false;
 
-								document.getElementById("LogouTProcess").innerHTML = "<br /><br /><font class=\"loading_text\"><?php echo _QXZ("LOGOUT PROCESS COMPLETE, YOU MAY NOW CLOSE YOUR BROWSER OR LOG BACK IN"); ?></font><br /><br />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <img src=\"./images/<?php echo _QXZ("agent_loading_done.gif"); ?>\" height=\"206px\" width=\"206px\" alt=\"<?php echo _QXZ("LOGOUT PROCESS COMPLETE, YOU MAY NOW CLOSE YOUR BROWSER OR LOG BACK IN"); ?>\" />";
+								document.getElementById("LogouTProcess").innerHTML = `<br><br><div style="text-align:center;color:#666;font-weight:600;"><?php echo _QXZ("LOGOUT PROCESS COMPLETE, YOU MAY NOW CLOSE YOUR BROWSER OR LOG BACK IN"); ?></div><br><img src="./images/<?php echo _QXZ("agent_loading_done.gif"); ?>" width="206px" height="206px">`;
+
 
 								agent_events('logged_out_complete', '', aec);   aec++;
 								}
