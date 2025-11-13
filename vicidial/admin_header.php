691dc3cfb757bd8467bf1b1d30f792c842ae1fd2
simpl <simpl@MacBookPro.lan>
2025-11-14 05:10:40 +0530

    admin_header_ui

--- vicidial/admin_header.php
+++ vicidial/admin_header.php
@@ -3227,7 +3227,7 @@ if ($subcamp_font_size < 4) {$subcamp_font_size='11';}
     </div>
   <?php 
   }
-  
+}
   ######################### FULL HTML HEADER END #######################################
   ?>
   
