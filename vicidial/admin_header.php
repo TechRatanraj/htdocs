dddf9001dfda81bf81a834997526fb76ee3ec4a6
simpl <simpl@MacBookPro.lan>
2025-11-13 18:56:26 +0530

    admin_header_ui

--- vicidial/admin_header.php
+++ vicidial/admin_header.php
@@ -2776,10 +2776,10 @@ if ($subcamp_font_size < 4) {$subcamp_font_size='11';}
   <!-- Main content area -->
 <span style="position:absolute;left:300px;top:30px;z-index:1;visibility:hidden;" id="audio_chooser_span"></span>
 
-<header style="position: fixed; top: 0; left: 0;
-    margin-left: 220px;
-    width: calc(100% - 220px);
-    background: linear-gradient(90deg, #<?php echo $SSmenu_background ?> 0%, #0b2447 100%);
+<header style="position: fixed; top: 0; left: 0; right: 0;
+    margin-left: 0;
+    width: 100%;
+    background: #0b2447;
     color: white;
     padding: 12px 24px;
     display: flex;
