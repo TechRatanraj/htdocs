f6590d19adaba60ca0548adfc5d0ded637e1bb74
simpl <simpl@MacBookPro.lan>
2025-11-15 21:00:26 +0530

    admin_header_ui

--- vicidial/admin.php
+++ vicidial/admin.php
@@ -28212,11 +28212,6 @@ if ($SSoutbound_autodial_active > 0) {
     }
     echo "</div>";
 
-    echo "</div>"; // end right column
-    echo "</div>"; // end grid
-    echo "</div>"; // end Dial Status section
-
-}
 
 			echo "<tr bgcolor=#$SSstd_row4_background><td align=right>"._QXZ("List Order Randomize").": </td><td align=left>";
 			if ($ALTmultiDISABLE > 0)
