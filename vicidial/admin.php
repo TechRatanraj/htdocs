7751ee11fd9228ceb989556f4bffa45aa5d51cd8
simpl <simpl@SIMPL-FVFHK4LNQ05N.local>
2025-11-25 16:39:58 +0530

    admin_header_ui

--- vicidial/admin.php
+++ vicidial/admin.php
@@ -28567,10 +28567,6 @@ if ($SScall_limit_24hour > 0) {
 
     echo "</div>"; // End grid container
     echo "</div>"; // End section container
-} else {
-    echo "<input type=hidden name=call_limit_24hour_method value='$call_limit_24hour_method'>";
-    echo "<input type=hidden name=call_limit_24hour_scope value='$call_limit_24hour_scope'>";
-    echo "<input type=hidden name=call_limit_24hour value='$call_limit_24hour'>";
 
 // to be started here 
 
