5dd67a966505ea64f263eeba3d8db877234a11a0
simpl <simpl@MacBookPro.lan>
2025-11-29 16:54:32 +0530

    admin_header_ui

--- vicidial/user_stats.php
+++ vicidial/user_stats.php
@@ -1331,7 +1331,7 @@ else {
             color: white;
             padding: 20px;
             border-radius: 10px;
-            margin-bottom: 25px;
+            margin-bottom: 15px;
             box-shadow: 0 4px 6px rgba(0,0,0,0.1);
         }
         
